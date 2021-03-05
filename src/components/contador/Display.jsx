254d493a8369c5406e8a8032f9ef28fa53eb7a7b
Alexsandre Almeida <alexsandrer@gmail.com>
2021-03-05 18:02:32 -0300

    Separaçao do componente Contador em vários componentes

---
+++ src/components/contador/Display.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// eslint-disable-next-line
+export default props => {
+    return (
+        <div>
+            <h3>Valor Atual: {props.numero}</h3>
+        </div>
+    )
+}
