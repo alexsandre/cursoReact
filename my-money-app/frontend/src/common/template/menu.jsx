746339c5cc7a13b22741c332c7b61d6821f2157d
Alexsandre Almeida <alexsandrer@gmail.com>
2021-03-29 18:56:39 -0300

    Altera o menu item para usar o componente Link do react-router

--- my-money-app/frontend/src/common/template/menu.jsx
+++ my-money-app/frontend/src/common/template/menu.jsx
@@ -4,11 +4,11 @@ import MenuTree from './menuTree'
 
 export default props => (
     <ul className='sidebar-menu'>
-    <MenuItem path='#' label='Dashboard' icon='dashboard' />
-        <MenuItem path='#dashboard2' label='Dashboard 2' icon='dashboard' />
+    <MenuItem path='/' label='Dashboard' icon='dashboard' />
+        <MenuItem path='dashboard2' label='Dashboard 2' icon='dashboard' />
         <MenuTree label='Cadastro' icon='edit'>
-            <MenuItem path="#billingCycles" label="Ciclos de Pagamentos" icon="usd" />
-            <MenuItem path="#" label="Logout" icon="sign-out" />
+            <MenuItem path="billingCycles" label="Ciclos de Pagamentos" icon="usd" />
+            <MenuItem path="/" label="Logout" icon="sign-out" />
         </MenuTree>
     </ul>
 )
